c4ec1fba1b4fbfb6cd5fb79a26f9c839efa37f8b
kuand <kuand3203@gmail.com>
2022-10-09 02:10:02 +0300

    rm bug

--- app/bot/handlers/action/static_text.py
+++ app/bot/handlers/action/static_text.py
@@ -2,6 +2,7 @@ proxy_choose_msg_names = [
     'Купить Proxy', 'Выбрать страну прокси', 'Выбрать срок прокси',
     'Выбрать тип прокси', 'Выбрать количество прокси'
 ]
+proxy_version_name = 'Выбрать тип прокси'
 proxy_order_fields = [
     'version', 'country', 'period', 'ptype', 'count'
 ]
