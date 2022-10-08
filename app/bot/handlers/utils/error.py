c4e66a9afb7173fd7c339fe1c796aeab551610df
kuand <kuand3203@gmail.com>
2022-10-09 01:40:47 +0300

    rm bug

--- app/bot/handlers/utils/error.py
+++ app/bot/handlers/utils/error.py
@@ -8,34 +8,25 @@ from telegram.ext import CallbackContext
 
 from abridge_bot.settings import TELEGRAM_LOGS_CHAT_ID
 from bot.models import User
+from bot.handlers.utils import utils
 
 
 def send_stacktrace_to_tg_chat(update: Update, context: CallbackContext) -> None:
     u = User.get_user(update, context)
-
     logging.error("Exception while handling an update:", exc_info=context.error)
+    
+    next_state, prev_message_id = User.get_broadcast_next_states(u.user_id, message_id=36)
+    message_id = utils.send_broadcast_message(
+        next_state, u.user_id, prev_message_id
+    )
+    User.set_message_id(u.user_id, message_id)
 
     tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
     tb_string = ''.join(tb_list)
-
-    # Build the message with some markup and additional information about what happened.
-    # You might need to add some logic to deal with messages longer than the 4096 character limit.
     message = (
         f'Возникло исключение при обработке обновления\n'
         f'<pre>{html.escape(tb_string)}</pre>'
     )
-
-    user_message = """
-😔 Внутри бота что-то сломалось.
-Это потому, что мы постоянно улучшаем наш сервис, но иногда мы можем забыть протестировать некоторые базовые вещи.
-Мы уже получили все детали для решения проблемы.
-Вернуться к /start
-"""
-    context.bot.send_message(
-        chat_id=u.user_id,
-        text=user_message,
-    )
-
     admin_message = f"⚠️⚠️⚠️ for {u.tg_str}:\n{message}"[:4090]
     if TELEGRAM_LOGS_CHAT_ID:
         context.bot.send_message(
